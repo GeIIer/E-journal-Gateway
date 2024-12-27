482080545c783b8ceabd39501250225a9cb0f52c
ivanovad <ivanov@o-code.ru>
2024-12-27 11:59:49 +0400

    init

---
+++ src/main/java/com/example/ejournalgateway/conf/DiscoveryConfig.java
@@ -0,0 +1,34 @@
+package com.example.ejournalgateway.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class DiscoveryConfig {
+
+    @Value("${FRONTEND_URL:http://127.0.0.1:4200}")
+    private String frontendUrl;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        if (frontendUrl.endsWith("/")) {
+            frontendUrl = frontendUrl.substring(0, frontendUrl.length()-1);
+        }
+        return builder.routes()
+                .route("e-journal-auth", r -> r.path("/api/v1/auth/**")
+                        .uri("lb://e-journal-auth"))
+                .route("e-journal-back", r -> r.path("/api/v1/journal/**")
+                        .uri("lb://e-journal-back"))
+                .route("frontend-service", r -> r.path("/**")
+                        .uri(frontendUrl))
+                .route("frontend-service", r -> r.path("/static/**")
+                        .uri("%s/static".formatted(frontendUrl)))
+                .build();
+    }
+
+}
---
+++ src/test/java/com/example/ejournalgateway/EJournalGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejournalgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EJournalGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
