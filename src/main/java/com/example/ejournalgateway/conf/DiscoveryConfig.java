d9df3ea6cb55abf6490b295bbaa31b190e83abe7
ivanovad <ivanov@o-code.ru>
2024-12-28 13:22:57 +0400

    fix

--- src/main/java/com/example/ejournalgateway/conf/DiscoveryConfig.java
+++ src/main/java/com/example/ejournalgateway/conf/DiscoveryConfig.java
@@ -6,27 +6,19 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.ResourceHandlerRegistry;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableDiscoveryClient
+@EnableWebFlux
 public class DiscoveryConfig {
     @Value("${FRONTEND_URL:http://localhost:8080}")
     private String frontendUrl;
-
-//    @Bean
-//    public RouterFunction<ServerResponse> staticResourceRouter() {
-//        return RouterFunctions.resources("/**", new ClassPathResource("static/"));
-//    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -35,13 +27,9 @@ public class DiscoveryConfig {
         }
         return builder.routes()
                 .route("e-journal-auth", r -> r.path("/api/v1/auth/**")
-                        .uri("http://e-journal-auth"))
+                        .uri("lb://e-journal-auth"))
                 .route("e-journal-back", r -> r.path("/api/v1/journal/**")
-                        .uri("http://e-journal-back"))
-//                .route("frontend-service", r -> r.path("/**")
-//                        .uri(frontendUrl))
-//                .route("frontend-service", r -> r.path("/static/**")
-//                        .uri("%s/static".formatted(frontendUrl)))
+                        .uri("lb://e-journal-back"))
                 .build();
     }
 
